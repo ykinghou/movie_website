f98b62810429ef596cffaea6c7580554453320e3
yking,hou <hyking@hotmail.com>
2017-04-01 17:48:51 +0800

    add order by created_at to group's post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401090933) do
+ActiveRecord::Schema.define(version: 20170401094419) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 20170401090933) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
